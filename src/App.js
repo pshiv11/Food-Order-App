e206757985fe6a34bd610d176b5083bf1441a594
pshiv11 <shivang.patel1@outlook.com>
2021-12-02 18:58:05 -0500

    added backdrop & Overlay in Modal and Modal in Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import  React  from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header></Header>
+      { cartIsShown && <Cart onClose={hideCartHandler}></Cart>}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals></Meals>
       </main>
