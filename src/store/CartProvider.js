25b3bd029a013580cdd8befcbfe5ecebb3580d80
pshiv11 <shivang.patel1@outlook.com>
2021-12-06 21:45:18 -0500

    add/remove to&from cart is implemented

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -38,6 +38,30 @@ const cartReducer = (state, action) => {
         };
     }
 
+    if(action.type === 'REMOVE'){
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+        if(existingItem.amount === 1){
+            updatedItems = state.items.filter(item => item.id !== action.id );  //all items where id's are not equal to action.id are kept and the one that match is deleted
+        }
+        else{
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+
+    }
+
+    return defaultCartState;
+
 }
 
 
